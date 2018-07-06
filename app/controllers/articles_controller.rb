e5a4fade60f1b30dbc1f4eff3b1ad6c70111e036
GAKU <gemini.05211124@gmail.com>
2018-07-07 08:02:24 +0900

    Tweet関連の作業

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,21 @@
 class ArticlesController < ApplicationController
   def new
+    @article = Article.new
   end
   def create
     @article = Article.new(article_params)
 
-  @article.save
-  redirect_to @article
+  if @article.save
+   redirect_to @article
+  else
+   render 'new'
+  end
   end
   def show
     @article = Article.find(params[:id])
-
+  end
+  def index
+  @articles = Article.all
   end
   private
   def article_params
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+  validates :text, length: { maximum: 25 }
 end
