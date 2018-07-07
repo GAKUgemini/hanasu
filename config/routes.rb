2f8a9e9f691c942f02e5ffba02d7d950fb15f8e7
GAKU <gemini.05211124@gmail.com>
2018-07-08 08:33:01 +0900

    ツイート一覧画面生成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'users/new'
   get 'pages/index'
+  get 'pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#top'
   resources :users
